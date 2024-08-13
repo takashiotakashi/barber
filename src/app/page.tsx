b208d16489c8cfe1cc98eb7b3db68252d5ac9336
Carlos Esbeltte <carlosmechelli@gmail.com>
2024-08-13 07:29:19 -0300

    feat: ensure barbershops can be searched by title and service

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,7 @@ import BarbershopItem from "./_components/barbershop-item"
 import { quickSearchOptions } from "./_constantes/search"
 import BookingItem from "./_components/booking-item"
 import Search from "./_components/search"
+import Link from "next/link"
 
 // PRIMEIRA LETRA : X
 //SEGUNDA LETRA : 7
@@ -38,14 +39,21 @@ const Home = async () => {
         {/* BUSCA RÁPIDA */}
         <div className="mt-6 flex gap-3 overflow-x-scroll [&::-webkit-scrollbar]:hidden">
           {quickSearchOptions.map((option) => (
-            <Button className="gap-2" variant="secondary" key={option.title}>
-              <Image
-                src={option.imageUrl}
-                width={16}
-                height={16}
-                alt={option.title}
-              />
-              {option.title}
+            <Button
+              className="gap-2"
+              variant="secondary"
+              key={option.title}
+              asChild
+            >
+              <Link href={`/barbershops?service=${option.title}`}>
+                <Image
+                  src={option.imageUrl}
+                  width={16}
+                  height={16}
+                  alt={option.title}
+                />
+                {option.title}
+              </Link>
             </Button>
           ))}
         </div>
